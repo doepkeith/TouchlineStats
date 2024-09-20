b2f4305cf70f47705721698cac0236e16d102b18
Ben <bendoepk@gmail.com>
2024-09-20 14:34:29 -0400

    removed default images and began front-end

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,49 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import {SafeAreaView, View, Text, Button, StyleSheet, Image, Platform, Dimensions, Appearance} from 'react-native';
+
+const userColorScheme = Appearance.getColorScheme();
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView style={[styles.container, (userColorScheme === 'light' ? styles.defaultLight : styles.defaultDark)]}>
+            <Image 
+                style={(Platform.OS === 'ios' || Platform.OS === 'android') ? styles.imagePhone : styles.image}
+                source={require('../assets/images/TouchlineStatsLogo.png')}
+            />
+        </SafeAreaView>
+    )
 }
+
+export const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1
+    },
+    defaultLight: {
+        backgroundColor: "white",
+    },
+    defaultDark: {
+        backgroundColor: "gray",
+    },
+    defaultDarkText: {
+        color: "white",
+        fontWeight: "900",
+        fontSize: 100
+    },
+    defaultLightText: {
+        color: "black",
+        fontWeight: "900",
+        fontSize: 100
+    },
+    image: {
+        height: 500,
+        width: 500,
+        resizeMode: 'contain'
+    },
+    imagePhone: {
+        width: (Dimensions.get('window').width - 10),
+        height: (Dimensions.get('window').width - 10),
+        resizeMode: 'contain'
+    }
+});
